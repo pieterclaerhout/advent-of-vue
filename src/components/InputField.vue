f9e4f9bd2af76337e31d5f08005557834268eae3
Pieter Claerhout <pieter@contractify.be>
2022-12-10 22:07:44 +0100

    Day5 updates

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -6,6 +6,8 @@ const props = defineProps({
   autofocus: Boolean,
 });
 
+const emit = defineEmits(["update:modelValue"]);
+
 const defaultField = ref<HTMLInputElement | null>(null);
 
 onMounted(() => {
@@ -13,13 +15,17 @@ onMounted(() => {
     defaultField.value?.focus();
   }
 });
+
+const emitEvent = (event: Event) => {
+  emit("update:modelValue", (event.target as HTMLInputElement).value);
+};
 </script>
 
 <template>
   <input
     ref="defaultField"
     :value="modelValue"
-    @input="(event) => $emit('update:modelValue', event.target.value)"
+    @input="emitEvent"
     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
   />
 </template>
