43fff568ca89f4182ede4a3ed4b1f826b63cab0f
Pieter Claerhout <pieter@contractify.be>
2022-12-11 18:51:52 +0100

    Added support for overriding the headers

--- src/clients/BaseClient.ts
+++ src/clients/BaseClient.ts
@@ -1,12 +1,30 @@
 type Params = { [key: string]: string };
 
+interface HeadersFunc {
+  (headers: Headers): void;
+}
+
 export class BaseClient {
   protected apiHost: string = "";
+  public headers?: HeadersFunc;
 
-  protected async fetch(url: string = "", params: Params = {}): Promise<any> {
+  protected async fetch(
+    url: string = "",
+    params: Params = {},
+    method: string = "GET"
+  ): Promise<any> {
     const qs = new URLSearchParams(params);
 
-    const resp = await fetch(`${this.apiHost}${url}?${qs}`);
+    const headers: Headers = new Headers();
+    if (this.headers) {
+      await this.headers(headers);
+    }
+
+    const resp = await fetch(`${this.apiHost}${url}?${qs}`, {
+      method: method,
+      headers: headers,
+    });
+
     const result = await resp.json();
     if (!resp.ok) {
       throw new Error(result);
